cc08923bf74e53f51f0f8fe54e129115bcb65314
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-07-02 15:56:10 -0700

    Add files via upload

---
+++ src/ecstsy/AdvancedEnchantments/Enchantments/CEGroups.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace ecstsy\AdvancedEnchantments\Enchantments;
+
+use ecstsy\AdvancedEnchantments\utils\Utils;
+use pocketmine\utils\TextFormat as C;
+
+final class CEGroups {
+
+    private static array $groups = [];
+    private static ?string $fallbackGroup = null;
+
+    public static function init(): void {
+        $config = Utils::getConfiguration("groups.yml");
+        $groupsConfig = $config->get("groups", []);
+        self::$fallbackGroup = strtoupper($config->getNested("settings.fallback-group", "SIMPLE"));
+
+        $id = 1;  
+        foreach ($groupsConfig as $groupName => $groupData) {
+            self::$groups[strtoupper($groupName)] = [
+                'id' => $id,
+                'global_color' => $groupData['global-color'] ?? "",
+                'group_name' => $groupData['group-name'] ?? "",
+                'item' => $groupData['item'] ?? [],
+                'slot_increaser' => $groupData['slot-increaser'] ?? [],
+                'magic_dust' => $groupData['magic-dust'] ?? []
+            ];
+            $id++;  // Increment ID for the next group
+        }
+    }
+
+    public static function getGroupData(string $groupName): ?array {
+        return self::$groups[strtoupper($groupName)] ?? self::$groups[self::$fallbackGroup] ?? null;
+    }
+
+    public static function getFallbackGroup(): string {
+        return self::$fallbackGroup;
+    }
+
+    public static function getGroupId(string $groupName): ?int {
+        $groupData = self::getGroupData($groupName);
+        return $groupData['id'] ?? null;
+    }
+
+    public static function translateGroupToColor(int $groupId): string {
+        foreach (self::$groups as $groupName => $groupData) {
+            if ($groupData['id'] === $groupId) {
+                return C::colorize($groupData['global_color']);
+            }
+        }
+        // Return the color for the fallback group if the rarity does not match any group
+        $fallbackGroupData = self::getGroupData(self::getFallbackGroup());
+        return C::colorize($fallbackGroupData['global_color'] ?? "&7");
+    }
+
+    public static function getGroupName(int $groupId): ?string {
+        foreach (self::$groups as $groupName => $groupData) {
+            if ($groupData['id'] === $groupId) {
+                return $groupData['group_name'];
+            }
+        }
+        // Return the name for the fallback group if the rarity does not match any group
+        $fallbackGroupData = self::getGroupData(self::getFallbackGroup());
+        return $fallbackGroupData['group_name'] ?? "Unknown";
+    }
+}
