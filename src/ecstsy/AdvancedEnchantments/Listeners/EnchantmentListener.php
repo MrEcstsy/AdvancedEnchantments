3180e8a90021547c2d0a3e2ff14208c9ef88d811
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-07-03 18:34:34 -0700

    registered more effect types but not implemented
    also added checkConditions method with 2 conditions so far.

--- src/ecstsy/AdvancedEnchantments/Listeners/EnchantmentListener.php
+++ src/ecstsy/AdvancedEnchantments/Listeners/EnchantmentListener.php
@@ -182,7 +182,149 @@ class EnchantmentListener implements Listener {
                 case 'BURN':
                     // set attackers on fire
                     break;
+                case 'ADD_FOOD':
+                    break;    
+                case 'REMOVE_FOOD':
+                    break;
+                case 'VEINMINER':
+                    break;
+                case 'TP_DROPS':
+                    // Teleport drops to players inventory sorta like auto pickup
+                    break;          
+                case 'GAURD':
+                    // Summon a mob on defense
+                    break;      
+                case 'INCREASE_DAMAGE':
+                    // Increase damage, need to implement a condition check so it can be made to work for e.g with zombies, or players or whichever mob
+                    break;    
+                case 'PULL_CLOSER':
+                    // Pulls closer to victim
+                    break;
+                case 'LIGHTNING':
+                    // Strike lighting at the entity
+                    break;
+                case 'EXTINGUISH':
+                    // Removes fire from player, or entity (removes fire from using LIGHTNING type)
+                    break;
+                case 'ADD_DURABILITY':
+                    // Not sure if its possible to do this anymore?
+                    break;             
+                case 'BOOST':
+                    // Launch entity or player into the air e.g launching victim into air when low hp
+                    break;
+                case 'TELEPORT_BEHIND':
+                    // Teleport behind entity / player
+                    break;     
+                case 'BREAK_TREE':
+                    // Breaks an entire tree
+                    break;
+                case 'ADD_HEALTH':
+                    // Add health to player
+                    break;
+                case 'CURE':
+                    // Remove a bad potion effect
+                    break;
+                case 'NEGATE_DAMAGE':
+                    // Negate damage
+                    break;
+                case 'BREAK_BLOCK':
+                    // Breaks blocks in radius
+                    break;       
+                case 'DISABLE_ACTIVATION':
+                    // Prevents an enchantment from activating
+                    break;
+                case 'DECREASE_DAMAGE':
+                    // Decrease damage
+                    break;
+                case 'ADD_DURABILITY_CURRENT_ITEM':
+                    // When item breaks the item that has the enchantment with this effect will be remove the enchantment to restore the item to full durability, might have to check when the durability is low e.g at 1
+                    break;            
+                case 'REMOVE_ENCHANT':
+                    // Removes an enchantment from an item
+                    // used for the example above and for any other reason ppl can be creative...
+                    break;   
+                case 'FIREBALL':
+                    // Arrows turn into fireballs 
+                    break;
+                case 'RESET_COMBO':
+                    // Resets combo
+                    break;
+                case 'KILL':
+                    // Not sure how this would work, but would kill x amount of entities in a mob stack
+                    break;
+                case 'STEAL_HEALTH':
+                    break;
+                case 'HALF_DAMAGE':
+                    // Intended to make attacker do half the damage, can be paired with 'ADD_DURABILITY_CURRENT_ITEM' to make an enchant that does 'half damage' in exchange for repairing the item
+                    break;           
+                case 'REPAIR':
+                    break;
+                case 'REMOVE_RANDOM_ARMOR':
+                    break;
+                case 'SPAWN_ARROWS':
+                    // Spawn arrows over opponent
+                    break;         
+                case 'KEEP_ON_DEATH':
+                    break;
+                case 'DISARM':
+                    break;
+                case 'REVIVE':
+                    // 'Revive' the target when killed
+                    break;              
+                case 'PUMPKIN':
+                    // Show the pumpin vignette to the target
+                    break;                  
+                case "STOP_KNOCKBACK":
+
+                    break;
+                case 'REMOVE_SOULS':
+
+                    break;
+                case 'MORE_DROPS':
+
+                    break;      
+                case 'SHUFFLE_HOTBAR':
+
+                    break;           
+                case 'ADD_SOULS':
+                        
+                    break;
             }
         }
     }
+
+    public static function checkConditions(array $conditions, Player $player, Entity $victim): bool {
+        foreach ($conditions as $condition) {
+            $type = $condition['type'] ?? null;
+            $conditionMode = $condition['condition_mode'] ?? 'allow';
+    
+            if ($type === 'VICTIM_HEALTH') {
+                $greaterThan = $condition['greater-than'] ?? 0;
+                if ($conditionMode === 'stop' && $victim->getHealth() > $greaterThan) {
+                    return false; // Stop if victim's health is greater than the specified value
+                } elseif ($conditionMode === 'allow' && $victim->getHealth() <= $greaterThan) {
+                    return true; // Allow if victim's health is less than or equal to the specified value
+                }
+            } elseif ($type === 'IS_SNEAKING') {
+                $target = $condition['target'] ?? 'self';
+                $value = $condition['value'] ?? false;
+                if ($target === 'self') {
+                    $isSneaking = $player->isSneaking();
+                } else {
+                    if ($victim instanceof Player) {
+                        $isSneaking = $victim->isSneaking();
+                    }
+                }
+                if ($conditionMode === 'allow' && $isSneaking === $value) {
+                    return true; // Allow if the target's sneaking status matches the value
+                } elseif ($conditionMode === 'stop' && $isSneaking !== $value) {
+                    return false; // Stop if the target's sneaking status does not match the value
+                }
+            }
+            
+
+        }
+        return true; 
+    }
+    
 }
