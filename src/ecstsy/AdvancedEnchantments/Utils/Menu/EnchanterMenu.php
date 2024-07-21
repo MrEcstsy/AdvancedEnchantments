07eb458f52b469444519db5eb64eb26c9c0e166e
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-07-21 04:02:41 -0700

    updated enchanter menu
    fixed form not sending
    
    refined confirmation code

--- src/ecstsy/AdvancedEnchantments/Utils/Menu/EnchanterMenu.php
+++ src/ecstsy/AdvancedEnchantments/Utils/Menu/EnchanterMenu.php
@@ -4,13 +4,14 @@ namespace ecstsy\AdvancedEnchantments\Utils\Menu;
 
 use ecstsy\AdvancedEnchantments\Utils\CustomSizedInvMenu;
 use ecstsy\AdvancedEnchantments\Utils\Utils;
+use ecstsy\essentialsx\Loader;
+use jojoe77777\FormAPI\CustomForm;
 use muqsit\invmenu\InvMenu;
 use muqsit\invmenu\transaction\DeterministicInvMenuTransaction;
 use pocketmine\item\StringToItemParser;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as C;
-use Vecnavium\FormsUI\CustomForm;
-use Vecnavium\FormsUI\SimpleForm;
+
 
 class EnchanterMenu {
 
@@ -85,7 +86,9 @@ class EnchanterMenu {
                 $sound = $slotConfig['sound'];
         
                 $player->removeCurrentWindow();
-                $player->sendForm(self::sendConfirmationForm($player, $enchantGroup, $eco, $price, $sound));
+                $transaction->then(function (Player $player) use ($enchantGroup, $eco, $price, $sound) {
+                    $player->sendForm(self::sendConfirmationForm($player, $enchantGroup, $eco, $price, $sound));
+                });
             }
         }));
           
@@ -95,48 +98,67 @@ class EnchanterMenu {
         return $menu;
     }
     
-    public static function sendConfirmationForm(Player $player, string $enchantGroup, string $eco, int $price, string $sound): CustomForm
-    {
-        $config = Utils::getConfiguration("menus/enchanter.yml");
-        $form = new CustomForm(function(Player $player, $data) use($enchantGroup, $eco, $price, $sound) {
-            if ($data === null) {
-                return true;
-            }
-    
-            // Check if the player has enough resources to purchase
-            if (self::canAfford($player, $eco, $price)) {
-                self::deductResources($player, $eco, $price);
-    
-                $book = Utils::createRCBook($enchantGroup);
-    
-                $player->getInventory()->addItem($book);
-    
-                Utils::playSound($player, $sound);
-    
-                $player->sendMessage(C::GREEN . "You have successfully purchased a " . $enchantGroup . " enchantment book!");
-            } else {
-                $player->sendMessage(C::RED . "You do not have enough resources to make this purchase.");
-            }
-        });
-    
-        $form->setTitle(C::colorize($config->getNested("confirmation-form.name")));
-        $confirmationText = $config->getNested("confirmation-form.confirm.text");
-
-        if ($confirmationText === null) {
-            $confirmationText = "{price} {paymentType}";
+public static function sendConfirmationForm(Player $player, string $enchantGroup, string $eco, int $price, string $sound): CustomForm
+{
+    $config = Utils::getConfiguration("menus/enchanter.yml");
+    $form = new CustomForm(function(Player $player, $data) use($enchantGroup, $eco, $price, $sound, $config) {
+        if ($data === null) {
+            return true;
         }
 
-        $buttonText = str_replace(
-            ['{price}', '{paymentType}'],
-            [number_format($price), $eco],
-            $confirmationText
-        );
+        $amount = (int) $data[1];
+        $amount = max(1, min(2304, $amount));
 
-        $form->addInput(C::colorize("test"), 1);
-        $form->addLabel(C::colorize($config->getNested("confirmation-form.cancel.text")));
-    
-        return $form;
-    }
+        $totalPrice = $price * $amount;
+
+        if (self::canAfford($player, $eco, $totalPrice)) {
+            $book = Utils::createRCBook($enchantGroup, $amount);
+            $inventory = $player->getInventory();
+
+            
+            self::deductResources($player, $eco, $totalPrice);
+            if ($inventory->canAddItem($book)) {
+                $inventory->addItem($book);
+
+                Utils::playSound($player, $sound);
+
+                $messages = $config->getNested("messages.successfull-purchase");
+                foreach ($messages as $message) {
+                $player->sendMessage(C::colorize(str_replace(
+                        ["{amount}", "{enchant-group}", "{price}", "{payment-type}"],
+                        [$amount, $enchantGroup, number_format($totalPrice), $eco],
+                        $message
+                    )));
+                }
+            } else {
+                $messages = $config->getNested("messages.inventory-is-full");
+                foreach ($messages as $message) {
+                    $player->sendMessage(C::colorize($message));
+                }
+            }
+        } else {
+            $messages = $config->getNested("messages.cannot-afford");
+            foreach ($messages as $message) {
+                $player->sendMessage(C::colorize(str_replace("{exp}", self::getBalanceFormatted($player, $eco), $message)));
+            }
+        }
+    });
+
+    $form->setTitle(C::colorize($config->getNested("confirmation-form.name")));
+    $confirmationText = $config->getNested("confirmation-form.accept.text");
+
+    $buttonText = str_replace(
+        ['{price}', '{paymentType}'],
+        [number_format($price), $eco],
+        $confirmationText
+    );
+
+    $form->addLabel(C::colorize($buttonText));
+    $form->addInput(C::colorize("&r&aEnter Amount:"), "1"); // Default value 1
+
+    return $form;
+}
+
     
     public static function canAfford(Player $player, string $eco, int $price): bool
     {
@@ -149,10 +171,28 @@ class EnchanterMenu {
                 // Implement the check for BedrockEconomy
                 break;
             case "essentialsx":
-                
+                return Loader::getPlayerManager()->getSession($player)->getBalance() >= $price;
                 break;
             default:
                 return false;
+        }
+    }
+
+    public static function getBalanceFormatted(Player $player, string $eco): string
+    {
+        switch ($eco) {
+            case "exp":
+                return number_format($player->getXpManager()->getCurrentTotalXp());
+            case "xplevel":
+                return number_format($player->getXpManager()->getXpLevel());
+            case "BedrockEconomy":
+                // Implement the check for BedrockEconomy
+                break;
+            case "essentialsx":
+                return number_format(Loader::getPlayerManager()->getSession($player)->getBalance());
+                break;
+            default:
+                return 0;
         }
     }
     
@@ -169,7 +209,7 @@ class EnchanterMenu {
                 // Implement the deduction for BedrockEconomy
                 break;
             case "essentialsx":
-                // Implement the deduction for your own plugin's economy
+                Loader::getPlayerManager()->getSession($player)->subtractBalance($price);
                 break;
         }
     }
